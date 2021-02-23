9aa98d1c4c456e8be66487525c8be8ddbba7933e
Thomas Moessner <t.moessner04@gmail.com>
2021-02-23 17:14:37 +0100

    max box ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ require "open-uri"
 #User.destroy_all
 
 puts "Creating users..."
-=begin 
+
 User.create!(first_name: "Baptiste", last_name: "Josse", age: 29, email: "baptiste@gmail.com", password: "secret", description: "Salut moi c'est Baptiste, j'aime l'aventure et le code !! Je peux devenir ton ami contre une petite compensation financière :) A pluusss ! " ,avatar_url:"https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/20292681_10211935177968045_3900832542794430335_n.jpg?_nc_cat=101&ccb=3&_nc_sid=0debeb&_nc_ohc=LepZDhv_flQAX9gRpzk&_nc_ht=scontent-cdt1-1.xx&oh=bcc447badba35e3684d3e335af05e112&oe=60524335", nickname: "Baptou")
 User.create!(first_name: "Thomas", last_name: "Moessner", age: 28, email: "thomas@gmail.com", password: "secret", avatar_url:"https://ca.slack-edge.com/T02NE0241-U01JASARRND-2749c28ba94d-512", nickname: "Thom")
 User.create!(first_name: "Cyrielle", last_name: "Gout", age: 25, email: "cyrielle@gmail.com", password: "secret", avatar_url:"https://ca.slack-edge.com/T02NE0241-U01J4HUUB6G-4bd88b49390d-512", nickname: "Cycy")
 User.create!(first_name: "Alexandre", last_name: "Madeleine", age: 21, email: "alex@gmail.com", password: "secret",description: "Salut, je suis un type plutôt cool, viens me rencontrer", avatar_url: "https://ca.slack-edge.com/T02NE0241-U01JD3D89CH-c479e937dfeb-512", nickname: "Alex")
-=end
+
 
 puts "Creating spots..."
 file = URI.open('https://subocea.com/uploads/media/default/0001/05/fe95bd48c535ca34168f1abb9bf71f3631e0d623.jpeg')
