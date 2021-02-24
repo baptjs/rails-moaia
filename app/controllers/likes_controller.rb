f7270f004acedb8d2ee96aa98cb8b535525c7808
Thomas Moessner <t.moessner04@gmail.com>
2021-02-24 17:03:41 +0100

    ok

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,51 @@
+class LikesController < ApplicationController
+
+  def index
+    @spots = []
+    Spot.all.each do |spot|
+      spot.likes.each do |like|
+        if like.user_id == current_user.id
+          @spots << spot
+        end
+      end
+    end
+
+    @markers = @spots.map do |spot|
+      {
+        lat: spot.latitude,
+        lng: spot.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { spot: spot }),
+        spotId: spot.id
+      }
+    end
+
+  end
+
+  def create
+    @like = Like.new(like_params)
+    @like.user_id = current_user.id
+    @like.save!
+    if params[:whereami] == "spots"
+      redirect_to spots_path
+    elsif params[:whereami] == "likes"
+      redirect_to likes_path
+    end
+
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+    if params[:whereami] == "spots"
+      redirect_to spots_path
+    elsif params[:whereami] == "likes"
+      redirect_to likes_path
+    end
+  end
+
+  def like_params
+    params.permit(:spot_id)
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_164702) do
-
+ActiveRecord::Schema.define(version: 2021_02_24_141149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +42,15 @@ ActiveRecord::Schema.define(version: 2021_02_22_164702) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo_url"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "spot_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_likes_on_spot_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -98,6 +106,8 @@ ActiveRecord::Schema.define(version: 2021_02_22_164702) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "likes", "spots"
+  add_foreign_key "likes", "users"
   add_foreign_key "reviews", "spots"
   add_foreign_key "reviews", "users"
   add_foreign_key "spots", "users"
