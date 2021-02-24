afe6b9ad3b06518a3dc29bd26cd258377a9e37e9
alexandre madeleine <madeleinealexandre0@gmail.com>
2021-02-24 16:16:53 +0100

    fin

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,28 @@
 class ReviewsController < ApplicationController
+
   def show
   end
 
   def new
+    @spot = Spot.find(params[:spot_id])
+    @review = Review.new
   end
+
+  def create
+  @review = Review.new(review_params)
+  @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:date , :content, :tips , rating: [])
+  end
+
 end
