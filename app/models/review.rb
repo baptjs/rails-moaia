4e3e4647ee0126874e32bd536555fa7168b6a637
Baptiste Josse <baptiste.josse@gmail.com>
2021-02-25 10:51:43 +0100

    show spot + seed

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :spot
   has_many :spottings, dependent: :destroy
-  has_many :fishes, through: :spotting
+  has_many :fishes, through: :spottings
 
   validates :date, presence: true
   validates :content, presence: true
