b999e59d5d3effa76eab7bc9812e71a838a24a92
Cyrielle Gout <cyriellexg@hotmail.fr>
2021-03-02 16:16:17 +0100

    user page modif

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,18 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :messages
   has_many :conversations, through: :messages
-  # has_one_attached :photo
+  # has_one_attached :photos
 
   # validates :nickname, presence: true, uniqueness: true
+  def photos
+    results = []
+    return results if self.reviews.empty?
 
-
+    self.reviews.each do |review|
+      review.photos.each do |photo|
+        results << photo
+      end
+    end
+    return results
+  end
 end
-
